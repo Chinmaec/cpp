6e8dd52f4f8554d924909b1ffcf95f25e3b4cabd
Chinmae Chittybabu <ccchinmae@gmail.com>
2026-06-09 22:17:47 +0530

    variable and data types

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,15 @@
 # include <iostream>
 
 int main() {
-    std::cout << "I like pizza!";
+
+    // This is a comment
+    /*this
+    is 
+    a 
+    multi line
+    comment */
+    std::cout << "I like pizza!" << '\n';
+    std::cout << "It's really good!" << '\n';
     return 0;
     
 }
---
+++ variable_data_type.cpp
@@ -0,0 +1,31 @@
+# include <iostream>
+
+int main() {
+    // integer (whole number)
+    int age = 21;
+    int year = 2024; 
+    int days = 7.6; 
+
+    // double (number with decimal)
+    double price = 10.99;
+    double gpa = 2.5; 
+    double temperature = 25.1;
+
+    // single character 
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    // boolean (True or False)
+    bool student = true; 
+    bool power = false;
+
+    // string (objects that represents a sequence of text)
+    std::string name = "Bro";
+    std::string day = "Friday"; 
+    std::string food = "Pizza"; 
+    std::string address = "123 Fake St.";
+    std::cout << address; 
+    return 0;
+    
+}
