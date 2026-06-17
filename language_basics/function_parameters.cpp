d8f339ccaa6913211b4d38874afeec86e2a2594f
Chinmae Chittybabu <ccchinmae@gmail.com>
2026-06-17 15:52:33 +0530

    rnd generator

---
+++ language_basics/function_parameters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string> 
+
+void names(std::string fname){
+    std::cout << fname << "\n";
+}
+
+int main(){
+    names("Chinmae");
+    names("Prithvi");
+    names("Chittybabu");
+    return 0;
+}
+
