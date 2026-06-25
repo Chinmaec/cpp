91917a9a500de346d633a2594b81136873b08922
Chinmae Chittybabu <ccchinmae@gmail.com>
2026-06-25 10:18:05 +0530

    rolling SMA

---
+++ project_04_rolling_SMA.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+std::vector<double> smas(std::vector<double> array);
+
+int main(){
+    std::vector<double> prices = {9,10,11,12};
+    std::vector<double> smas; 
+    int window = 3; 
+
+}
+
+std::vector<double> smas(std::vector<double> array){
+    std::vector <double> result;
+
+    for (int i = 1; i <= size, i++){}
+    
+}
