97e5a2d1e07323236420de76082bb8f001e89133
Chinmae Chittybabu <ccchinmae@gmail.com>
2026-06-29 19:39:38 +0530

    basic quiz game

---
+++ fun_games/quiz_game.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cctype> // to use touper
+
+int main(){
+
+    std::string questions[] = {"1. What year was C++ created?: ",
+                               "2. Who invented C++?: ",
+                               "3. What is the predecessor of C++?: ",
+                               "4. Is the Earth flat?"}; 
+    
+    std::string options[][4] = {{"A. 1969", "B. 1979", "C. 1985", "D. 1989"},
+                               {"A. Guido van Rossum", "B. Bjarne Stroustrup", "C. John Carmak", "D. Mark Zuckerburg"},
+                               {"A. C", "B. C+", "C. C--", "D. B++"},
+                               {"A. yes", "B. no", "C. sometimes", "D. what's a Earth?"}};
+
+    char answerKey[] = {'C', 'B', 'A', 'B'};
+
+    int size = sizeof(questions)/sizeof(questions[0]);
+
+    char guess; 
+    int score = 0; 
+
+    for (int i=0; i<size; i++){
+        std::cout << "****************************\n";
+        std::cout << questions[i] << '\n';
+        std::cout << "****************************\n";
+
+        for (int j=0; j<sizeof(options[i])/sizeof(options[i][0]); j++){
+            std::cout << options[i][j] << '\n';
+        }
+
+        std::cin >> guess; 
+        guess = toupper(guess);
+
+        if(guess == answerKey[i]){
+            std::cout << "CORRECT!\n";
+            score++; 
+        }
+        else{
+            std::cout << "WRONG\n";
+            std::cout << "Answer: " << answerKey[i] << "\n";
+        }
+    }
+
+    std::cout << "Correct Guesses: " << score << '\n'; 
+    std::cout << "# of Questions: " << size << '\n'; 
+    std::cout << "Score: " << (score/(double)size)*100 << " %" << '\n'; 
+
+    return 0;                            
+}
