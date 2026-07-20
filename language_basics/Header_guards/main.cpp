722b207e53bf193a65f282d3bb7adc377413d074
Chinmae Chittybabu <ccchinmae@gmail.com>
2026-07-20 15:54:50 +0530

    header guards

---
+++ language_basics/Header_guards/main.cpp
@@ -0,0 +1,8 @@
+#include "square.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << SquareArea(3) << " sq cm." ; 
+    return 0;
+}
---
+++ language_basics/Header_guards/square.h
@@ -0,0 +1,8 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+int SquareArea(int s){
+    return s*s; 
+}
+
+#endif
