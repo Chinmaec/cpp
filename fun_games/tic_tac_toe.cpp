433262e86a208e4cb5b5d9fe5cd5c95d243cfe89
Chinmae Chittybabu <ccchinmae@gmail.com>
2026-07-30 15:47:28 +0530

    added output guards and random number generator

--- fun_games/tic_tac_toe.cpp
+++ fun_games/tic_tac_toe.cpp
@@ -1,29 +1,144 @@
-// Rules : 
-// A player can put only a single letter X or O in the 3 x 3 grid in each chance.
-// Both players will get chances alternatively one after another till someone wins or draws.
-// To win this game, the player must create a horizontal, vertical, or diagonal line consisting of three same letters.
-// The game is drawn, if all grids are filled with X or O letters but no line is made.
-
 #include <iostream>
 #include <string>
+#include <limits>
+#include <random>
 
-void printBoard(char board[9]){
+void printBoard(char board[9]);
+void makeMove(char inp, int place, char board[9]);
+bool validMove(int place, char board[9]);
+bool isWin(char board[9]);
+int compMove(char board[9]);
+
+int main()
+{
+
+    char board[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    char user_val;
+    char comp_val;
+    int place;
+    bool ok;
+
+    std::cout << "Welcome to Tic-Tac-Toe!\n";
+    std::cout << "Would you like to be X or O ?\n";
+    std::cin >> user_val;
+
+    while (user_val != 'X' && user_val != 'O')
+    {
+        std::cout << "That is not a valid entry. Choose between X and O.\n";
+        std::cin >> user_val;
+    }
+
+    if (user_val == 'X')
+    {
+        comp_val = 'O';
+    }
+    else
+    {
+        comp_val = 'X';
+    }
+
+    std::cout << "Player: " << user_val << "\n";
+    std::cout << "Computer: " << comp_val << "\n";
+
+    do
+    {
+        std::cout << "Please choose a number between 1 to 9 in the board\n";
+        printBoard(board);
+        std::cin >> place;
+
+        if (std::cin.fail())
+        {
+            ok = false;
+            std::cout << "Please enter a number. \n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else
+        {
+            ok = validMove(place, board);
+        }
+    } while (!ok);
+
+    makeMove(user_val, place, board);
+    printBoard(board);
+
+    return 0;
+}
+
+void printBoard(char board[9])
+{
     std::cout << board[0] << " " << board[1] << " " << board[2] << "\n";
     std::cout << board[3] << " " << board[4] << " " << board[5] << "\n";
     std::cout << board[6] << " " << board[7] << " " << board[8] << "\n";
 }
 
-void makeMove(char inp, int place, char board[9]){
-    place -= 1; 
-    board[place] = inp; 
-
+void makeMove(char inp, int place, char board[9])
+{
+    place -= 1;
+    board[place] = inp;
 }
 
-int main()
+bool validMove(int place, char board[9])
+{
+    if (place > 9 || place < 1)
+    {
+        std::cout << place << " is not a valid entry. Please choose a number between 1 to 9\n";
+        return false;
+    }
+    else if (board[place - 1] == 'X' || board[place - 1] == 'O')
+    {
+        std::cout << place << " has already been taken. Pleasae choose another spot\n";
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int compMove(char board[9]){
+    // Define range
+    int min = 1;
+    int max = 9;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(min, max);
+
+    int value;
+
+    do {
+        value = distrib(gen);
+    } while (! validMove(value, board));
+
+    return value;
+}
+
+bool isWin(char board[9])
 {
 
-    char board[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    makeMove('X', 4, board); 
-    printBoard(board); 
-    return 0;
+    int winCombos[8][3] = {
+        {0, 1, 2}, // top row
+        {3, 4, 5}, // middle row
+        {6, 7, 8}, // bottom row
+        {0, 3, 6}, // left column
+        {1, 4, 7}, // middle column
+        {2, 5, 8}, // right column
+        {0, 4, 8}, // diagonal
+        {2, 4, 6}  // other diagonal
+    };
+
+    for (int i = 0; i < 8; i++)
+    {
+        int a = winCombos[i][0];
+        int b = winCombos[i][1];
+        int c = winCombos[i][2];
+
+        if (board[a] == board[b] &&
+            board[a] == board[c])
+        {
+            return true;
+        }
+    }
+    return false;
 }
