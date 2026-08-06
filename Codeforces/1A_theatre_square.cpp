eee83b7adb2a6a5bec0131f2abfc1d1c28d798c2
Chinmae Chittybabu <ccchinmae@gmail.com>
2026-08-06 15:07:59 +0530

    codeforces questions

---
+++ Codeforces/1A_theatre_square.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+
+    int m,n,a;
+
+    cout << "m: ";
+    cin >> m;
+
+    cout << "n: ";
+    cin >> n;
+
+    cout << "a: ";
+    cin >> a;
+
+    cout << "m=" << m << " n=" << n << " a=" << a << "\n";
+
+    
+
+    return 0; 
+}
